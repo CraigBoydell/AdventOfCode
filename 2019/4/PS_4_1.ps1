6629b7b8418d4a3d66b579a7929673911c0990e3
unknown <cboydell@temenos.com>
2019-12-04 22:57:53 +1100

    Added alternative Regex approaches

--- 2019/4/PS_4_1.ps1
+++ 2019/4/PS_4_1.ps1
@@ -3,7 +3,8 @@ function Test-NoDecrease {
   $ToTest = $ToTest.ToString()
   1..5 | ForEach-Object {if ($ToTest[$_] -ge $ToTest[$_-1]) {}else {$false}  } | select-object -unique
 }
-  
+
+#77 Seconds
 #246515..739105
 $Doubles = $(246515..739105 | Select-String -Pattern '(.)\1' -AllMatches).line
 $Valid = $Doubles | ForEach-Object {
@@ -13,4 +14,10 @@ $Valid = $Doubles | ForEach-Object {
       $Pass
   }
 }
+$Valid.count
+
+#8 Seconds
+$Doubles = $(246515..739105 | Select-String -Pattern '(.)\1' -AllMatches).line
+$Pattern = '(?:0(?=[0-9])|1(?=[1-9])|2(?=[2-9])|3(?=[3-9])|4(?=[4-9])|5(?=[5-9])|6(?=[6-9])|7(?=[7-9])|8(?=[8-9])|9(?=9)){5}(?:0(?<=[0-9])|1(?<=[1-9])|2(?<=[2-9])|3(?<=[3-9])|4(?<=[4-9])|5(?<=[5-9])|6(?<=[6-9])|7(?<=[7-9])|8(?<=[8-9])|9(?<=9))+'
+$Valid = ($Doubles | Select-String -Pattern $Pattern -AllMatches).line
 $Valid.count
--- 2019/4/PS_4_2.ps1
+++ 2019/4/PS_4_2.ps1
@@ -4,6 +4,7 @@ function Test-NoDecrease {
   1..5 | ForEach-Object {if ($ToTest[$_] -ge $ToTest[$_-1]) {}else {$false}  } | select-object -unique
 }
   
+#77 Seconds
 #246515..739105
 $Doubles = $(246515..739105 | Select-String -Pattern '(.)\1' -AllMatches).line
 $Valid = $Doubles | ForEach-Object {
@@ -15,7 +16,17 @@ $Valid = $Doubles | ForEach-Object {
 }
 $Valid.count
 
+#8 Seconds
+$Doubles = $(246515..739105 | Select-String -Pattern '(.)\1' -AllMatches).line
+$Pattern = '(?:0(?=[0-9])|1(?=[1-9])|2(?=[2-9])|3(?=[3-9])|4(?=[4-9])|5(?=[5-9])|6(?=[6-9])|7(?=[7-9])|8(?=[8-9])|9(?=9)){5}(?:0(?<=[0-9])|1(?<=[1-9])|2(?<=[2-9])|3(?<=[3-9])|4(?<=[4-9])|5(?<=[5-9])|6(?<=[6-9])|7(?<=[7-9])|8(?<=[8-9])|9(?<=9))+'
+$Valid = ($Doubles | Select-String -Pattern $Pattern -AllMatches).line
+$Valid.count
+
 $WithGroups = $Valid | Select-String -Pattern '(.)\1{2,5}' -AllMatches
 $WithDoublesNotGroups = $WithGroups | ForEach-Object {$Line = $_.Line; $_.Matches.Value | ForEach-Object {$Line = $Line.Replace($_,'')}; if ($($Line |select-string -Pattern '(.)\1')) {$_.line}}
 $WithoutGroups = $Valid | Select-String -Pattern '(.)\1{2,5}' -NotMatch
 $WithDoublesNotGroups.Count + $WithoutGroups.Count
+
+#Alternative
+$ResultsRegex = $Valid | Select-String -Pattern '(.)\1{2,5}|(.)\2' -AllMatches
+($resultsregex | Where-Object { ($_.matches.Groups | select-object -Expandproperty Length) -like 2}).count
